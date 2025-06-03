db29b43b33960566229c5d03ee73e2d07f07ecc1
nhw45 <nhw1124@naver.com>
2025-06-03 21:53:23 +0900

    adminpage 엔드포인트트

--- Back/preProcessing/File_Mangement.py
+++ Back/preProcessing/File_Mangement.py
@@ -2,13 +2,47 @@ import os
 from fastapi import UploadFile, File
 import shutil
 
-def Delete_PreCSVFiles():
-    filepath="../precsv"
+def Delete_HospitalCSV():
+    filepath = "../precsv/hospitalFacilities.csv"
     if os.path.exists(filepath):
-        for file in os.scandir(filepath):
-                os.remove(file.path)
-        return "Remove ALL File"
-    else: return 'Directory Not Found'
+        os.remove(filepath)
+        return "hospitalFacilities.csv deleted"
+    else:
+        return "hospitalFacilities.csv not found"
+def Delete_OutingCSV():
+    filepath = "../precsv/outingFacilities.csv"
+    if os.path.exists(filepath):
+        os.remove(filepath)
+        return "outingFacilities.csv deleted"
+    else:
+        return "outingFacilities.csv not found"
+def Delete_PublicCSV():
+    filepath = "../precsv/publicFacilities.csv"
+    if os.path.exists(filepath):
+        os.remove(filepath)
+        return "publicFacilities.csv deleted"
+    else:
+        return "publicFacilities.csv not found"
+async def Upload_HospitalCSV(file):
+    save_path = "../precsv/hospitalFacilities.csv"
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"message": "hospitalFacilities.csv uploaded", "path": save_path}
+async def Upload_PublicCSV(file):
+    save_path = "../precsv/publicFacilities.csv"
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"message": "publicFacilities.csv uploaded", "path": save_path}
+async def Upload_OutingCSV(file):
+    save_path = "../precsv/outingFacilities.csv"
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"message": "outingFacilities.csv uploaded", "path": save_path}
+async def Upload_LeisureCSV(file):
+    save_path = "../csv/leisureFacilities.csv"
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"message": "leisureFacilities.csv uploaded", "path": save_path}
 async def Upload_Files(files):
     saved_files = []
     for file in files:
