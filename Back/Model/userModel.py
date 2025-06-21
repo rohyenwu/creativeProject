de3e724accc518b9e2a7061b3a515b7c15609e78
Junyeong-Ha <hth45123@naver.com>
2025-06-21 10:23:29 +0900

    기본위치 DB 연동 수정

--- Back/Model/userModel.py
+++ Back/Model/userModel.py
@@ -24,3 +24,25 @@ class UserModel:
                 return await cursor.fetchone()
         finally:
             await DBConnection.DBConnection.release_db_connection(conn)
+
+    @staticmethod
+    async def get_user_default_address(userID):
+        conn = await DBConnection.DBConnection.get_db_connection()
+        try:
+            query = "SELECT address, longitude, latitude FROM users WHERE userID = %s"
+            async with conn.cursor(aiomysql.DictCursor) as cursor:
+                await cursor.execute(query, (userID,))
+                return await cursor.fetchone()
+        finally:
+            await DBConnection.DBConnection.release_db_connection(conn)
+
+    @staticmethod
+    async def update_user_default_address(userID, address, latitude, longitude):
+        conn = await DBConnection.DBConnection.get_db_connection()
+        try:
+            query = "UPDATE users SET address = %s, latitude = %s, longitude = %s WHERE userID = %s"
+            async with conn.cursor(aiomysql.DictCursor) as cursor:
+                await cursor.execute(query, (address, latitude, longitude, userID))
+            await conn.commit()
+        finally:
+            await DBConnection.DBConnection.release_db_connection(conn)
